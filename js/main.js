c5399393623a21cd4df68ec18c0a7992a22afe64
Tizzio <adriano.tummi@hotmail.it>
2014-11-08 23:04:26 +0100

    first release

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ function StartEditor(){
 
 function Editor(){  
 	//working area 
-	
-	this.rects = [];
-	
+	 
 	this.textures = [];
 	 
-	this.textureSize= 256;
+	this.sprites = [];
+	this.spr_name_index = 0;
+	this.textureSize= 512;
 	this.spacing = 1;
 	
 	this.SetSize = function(){
@@ -76,52 +76,199 @@ function Editor(){
 		$("help").style.display="block";
 	}
 	
+	$("addSprite").onclick = function(){
+		editor.AddSprite(); 
+	}
+	
+	this.NameExists = function(name){
+		for(var i = 0; i < editor.sprites.length; i++){
+			if(editor.sprites[i].name == name)
+				return true;
+		}
+		return false;
+	}
+	
+	this.AddSprite = function(){
+		var spr = {
+			name: "sprite" + this.spr_name_index++,
+			width: 0,
+			height: 0,
+		}; 
+		spr.rects = [];
+		editor.sprites.push(spr);
+		
+		var p = document.createElement('p');
+		p.style.margin.top = "10px";
+		 
+		var name = document.createElement('input');
+		name.type = "text";
+		name.value = spr.name; 
+		name.sprite = spr;
+		name.onkeyup = function(e){
+			console.log(e.keyCode );
+			if(!e.ctrlKey && e.keyCode != 17){
+				start = this.selectionStart;
+				var str = this.value.replace(/[^a-zA-Z0-9_]/g, '');
+				if(!editor.NameExists(str)){
+					this.sprite.name = str;
+				}
+				this.value = str;
+				this.selectionStart = start;
+				this.selectionEnd = start;
+			}
+		} 
+		
+		
+		name.onchange = function(e){
+			if(!editor.NameExists(this.sprite.name)){
+				this.sprite.name = this.value;
+			}else{
+				this.value = this.sprite.name;
+				
+			}
+		}
+		
+		var container = document.createElement('p'); 
+		container.sprite = spr;
+		
+		var finput=document.createElement('input');
+		finput.type="file";
+		finput.multiple = true;
+		finput.style.display = "none";
+		finput.sprite = spr;
+		finput.container = container;
+		finput.onchange = function(){
+			editor.OnChangeFile(this);
+		}
+		
+		
+		var input=document.createElement('input'); 
+		input.type="button";
+		input.value = "add frame"; 
+		input.finput = finput;
+		input.onclick = function(){
+			finput.click();
+		}
+		
+		
+		
+		$("imagesContainer").appendChild(p);
+		p.appendChild(name);
+		p.appendChild(finput);
+		p.appendChild(input);
+		p.appendChild(container);
+		
+		
+		var div = document.createElement('div');
+		div.style.clear="both";
+		div.style.border = "0";
+		container.appendChild(div);
+		
+	}
+	
+	
+	//Load image file
+	 
+	this.OnChangeFile = function(element){ 
+	 
+		for(var i=0; i<element.files.length; i++){
+			var file = element.files[i]; 
+			
+			var fr = new FileReader();
+			fr.name = file.name;
+			 
+			fr.onload = function(){
+			
+				var extension = file.name.substr(file.name.lastIndexOf('.') + 1).toLowerCase();
+ 
+				if(extension == "png" || extension  == "jpg"  || extension  == "jpeg"  || extension  == "bmp" ){
+				
+					//image and rect for the atlas
+					var img = new Image();
+					img.src = this.result;
+					
+					if(element.sprite.width == 0 || element.sprite.height == 0){
+						element.sprite.width = img.width;
+						element.sprite.height = img.height;
+					}
+					if( element.sprite.width == img.width && element.sprite.height == img.height){
+						img.name = this.name;
+						var rect = new Rect(img);
+						rect.sprite = element.sprite;
+						element.sprite.rects.push(rect); 
+						
+						//preview image
+						var img = new Image();
+						img.src = this.result; 
+						img.width = Math.min(48, img.width);
+						img.height = Math.min(48, img.height);
+						img.style.border = "1px dotted black"; 
+						element.container.appendChild(img); 
+					}else{
+						alert(this.name + " has wrong sizes\n you need: "+element.sprite.width+"x"+element.sprite.height);
+					}
+				}
+				
+			};
+			fr.readAsDataURL(file);
+		}
+	}  
+	   
+ 
 	this.Export = function(){
-		var imagesRects = this.rects.slice();
-		this.textures = [];
+
+		var imagesRects = [];
+		//add images
+		for(var i = 0; i < editor.sprites.length; i++){
+			Array.prototype.push.apply(imagesRects, editor.sprites[i].rects); 
+		}
+		console.log(imagesRects);
+		//atlas textures to export
+		this.textures = []; 
+		
+		//destroy old textures
 		while (this.div.firstChild) {
 			this.div.removeChild(this.div.firstChild);
 		}
 		
-		for(var i = 0; i< this.rects.length; i++){
-			var rect = this.rects[i];
+		//setup rects
+		for(var i = 0; i< imagesRects.length; i++){
+			var rect = imagesRects[i];
 			rect.fit = undefined;
+			//get rect index, for the sorting
+			rect.index = rect.sprite.rects.indexOf(this);
+			//check rect size
 			if(rect.w > this.textureSize || rect.h > this.textureSize){
 				alert("image '"+rect.image.name+"' is too big for the choosen texture size ("+this.textureSize+"px)");
 				return 0;
 			} 
 		}
-		
-		
-		
-		this.rects.sort(function(a, b) {return a.area - b.area})
-		
-		
+		//sort and pack
+		imagesRects.sort(function(a, b) {return a.area - b.area});
 		var packer = new Packer(this.textureSize, this.textureSize);
     
-	
-		var output = {
-			width: this.textureSize,
-			height: this.textureSize,
-			
-		};
+		packer.init(this.textureSize, this.textureSize);
+		packer.fit(imagesRects);
 		
-		//render
-		var iter = 0; 
-		var atlasCount = 0;
-
-		while(this.rects.length > 0){
-			
-			packer.init(this.textureSize, this.textureSize);
-			packer.fit(this.rects);
+		var error = false;
+		for(rect in imagesRects)
+		{
+			if(rect.fit)
+			{
+				error = true;
+				break;
+			}
+		}
+		
+		if( !error)
+		{
+			//render   
 			var canvas = document.createElement('canvas'); 
 			canvas.width = this.textureSize;
 			canvas.height = this.textureSize;
 			canvas.ctx = canvas.getContext("2d");
 			canvas.style.margin = "5px";
-			canvas.style.border = "1px dotted black";
-			canvas.style.width = Math.min(this.textureSize, 150)+"px";
-			canvas.style.height = Math.min(this.textureSize, 150)+"px";
+			canvas.style.border = "1px dotted black"; 
 			this.textures.push(canvas);
 			this.div.appendChild(canvas);
 			  
@@ -130,73 +277,33 @@ function Editor(){
 			canvas.ctx.clearRect(0, 0, canvas.width, canvas.height); 
 			
 			 
-			var temp = []; 
-			while(this.rects.length > 0){
-				var rect = this.rects.pop();
+			while(imagesRects.length > 0){
+				var rect = imagesRects.pop();
+				canvas.ctx.drawImage(rect.image, offset+rect.fit.x, offset+rect.fit.y);  
+			}
+			
+			var str = "";
+			
+			for(var i = 0; i < editor.sprites.length; i++){
+				var spr = editor.sprites[i]; 
+				str += "s "+spr.name+" "+spr.rects.length +" "+spr.width +" "+spr.height +" 0 0\n";
 				
-				if(rect.fit){
-					canvas.ctx.drawImage(rect.image, offset+rect.fit.x, offset+rect.fit.y); 
-				}
-				else{
-					temp.push(rect); 
+				for(var j = 0; j < spr.rects.length; j++){
+					var rect = spr.rects[j];
+					str +=  (offset+rect.fit.x)+" "+(offset+rect.fit.y)+"\n";
 				}
 			}
-			this.rects = temp; 
 			
-			if(iter++ > 1000){
-				console.log("infinite loop!");
-				break; 
-			} 
-		}  
+			$("output").value = str;//"exported the void!";//JSON.stringify(this.verts);
+			$("saveFiles").style.display = "inline";
 		
-		 
-
-
-		this.rects = imagesRects; 
-		
-		$("output").value = "exported the void!";//JSON.stringify(this.verts);
-		
-		$("saveFiles").style.display = "inline";
-	}
-	 
-	
-	//Load image file
-	var finput = $("loadFile");
-	finput.onchange = function(){ 
-	
-		editor.rects = [];
-		$("imagesContainer").innerHTML = "";
-		for(var i=0; i<this.files.length; i++){
-			var file = this.files[i]; 
-			var fr = new FileReader();
-			fr.name = file.name;
-			fr.onload = function(){
-			
-				var extension = this.name.substr(this.name.lastIndexOf('.') + 1).toLowerCase();
- 
-				if(extension == "png" || extension  == "jpg"  || extension  == "jpeg"  || extension  == "bmp" ){
-				
-					//image and rect for the atlas
-					var img = new Image();
-					img.src = this.result;
-					img.name = this.name;
-					editor.rects.push(new Rect(img)); 
-					
-					//preview image
-					var img = new Image();
-					img.src = this.result;
-					var ratio = img.height/img.width;
-					img.width = 64;
-					img.height = 64*ratio;  
-					img.style.border = "1px dotted black";
-					$("imagesContainer").appendChild(img);
-				}
-				
-			};
-			fr.readAsDataURL(file);
 		}
-	} 
-	   
+		else
+		{
+			alert("Can't complete. select a bigger texture \n There are too many sprites for this texture");
+		}
+	}
+	  
 	 
 	function Rect(img){
 		this.image = img;
